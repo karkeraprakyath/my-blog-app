ccb77fe832ce6147537a51fbe65067b92b7fd8b2
prakyath-karkera <prakyathkarkera4@gmail.com>
2025-07-25 11:14:48 +0530

    seacrh bar updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-/** @format */
-
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./Pages/Home";
 import PostPage from "./Components/PostPage";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+
 function App() {
   return (
     <BrowserRouter basename='/my-blog-app'>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/blog/:id' element={<PostPage />} />
-      </Routes>
-      <Footer />
+      <div className="min-h-screen w-full flex flex-col overflow-x-hidden">
+        <Navbar />
+
+        <main className="flex-grow">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/blog/:id' element={<PostPage />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,6 +1,7 @@
 /** @format */
 
 import React from "react";
+import { IoSearchOutline } from "react-icons/io5";
 import Banner from "./../assets/logoforblogs.png";
 
 const tagsData = [
@@ -17,7 +18,7 @@ const tagsData = [
   "Python",
 ];
 
-function Search({ selectedTags, setSelectedTags }) {
+function Search({ selectedTags, setSelectedTags, search, setSearch }) {
   const handleSelectTag = (tag) => {
     if (tag === "All") {
       setSelectedTags([]);
@@ -37,7 +38,16 @@ function Search({ selectedTags, setSelectedTags }) {
           className='w-full h-auto md:h-[200px] rounded-2xl shadow-md object-cover'
         />
       </div>
-
+      <div className='relative mt-4 w-full max-w-[800px]'>
+        <IoSearchOutline className='absolute top-2.5 left-3 text-gray-900 text-md' />
+        <input
+          type='text'
+          placeholder='Search blogs...'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className='pl-10 pr-4 py-2 w-full rounded-md border border-gray-300 text-sm bg-white text-gray-800 placeholder:text-gray-800 focus:outline-none focus:ring-2 focus:ring-highlight'
+        />
+      </div>
       <div className='mt-6 w-full max-w-[800px] overflow-x-auto no-scrollbar'>
         <div className='flex gap-3 w-max px-2'>
           {tagsData.map((tag) => {
@@ -45,7 +55,6 @@ function Search({ selectedTags, setSelectedTags }) {
               tag === "All"
                 ? selectedTags.length === 0
                 : selectedTags.includes(tag);
-
             return (
               <button
                 key={tag}
